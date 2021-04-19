d87eac2804bd5ce8db228bec0eaf8a08936ca317
xjezzuuuu <xjezzuuuu@gmail.com>
2021-04-19 00:12:06 -0400

    Add back-end - Add route with results. - Fix bugs minors.

--- src/helpers/functions.js
+++ src/helpers/functions.js
@@ -1,10 +1,12 @@
+const { response } = require('express');
 const path = require('path');
 const fs = require('fs');
+let name;
 
 var Arr_Comunas = new Array();
 var Arr_Candidatos = new Array();
 var Arr_Preguntas = new Array();
-var Arr_Respuestas = new Array();
+var Arr_Respuestas;
 var Arr_Resultado = new Array();
 
 var cColR; //contador columna respuesta
@@ -25,6 +27,8 @@ function CargaBD() {
 			CP_Array(Arr_Lineas, Arr_Candidatos);
 			leeArchivo(path.join(__dirname, '../database/Preguntas.csv')).then((Arr_Lineas) => {
 				CP_Array(Arr_Lineas, Arr_Preguntas);
+				Arr_Respuestas = new Array(Arr_Preguntas.length - 1);
+				console.log('BD Cargadas...');
 			});
 		});
 	});
@@ -33,15 +37,16 @@ exports.CargaBD = CargaBD;
 //-----------------------------------------------------------------------------------------------------------------------------------------
 function ProcesaRespuestas(Respuestas) {
 	return new Promise(function (resolve, reject) {
-		leeArchivo(path.join(__dirname, '/Respuestas.csv')).then((Arr_Lineas) => {
-			console.log(Arr_Lineas.length);
-			if (Arr_Lineas.length > 0) {
-				CP_Array(Arr_Lineas, Arr_Respuestas);
-				RecomiendaCandidato().then((Respuesta) => resolve(Respuesta));
-			} else {
-				reject();
-			}
-		});
+		//leeArchivo(Respuestas).then( Arr_Lineas=> {
+		console.log(Respuestas.length);
+		if (Respuestas.length > 0) {
+			name = Respuestas[0];
+			CP_Array(Respuestas, Arr_Respuestas);
+			RecomiendaCandidato().then((Respuesta) => resolve(Respuesta));
+		} else {
+			reject(error);
+		}
+		//})
 	});
 }
 exports.ProcesaRespuestas = ProcesaRespuestas;
@@ -100,7 +105,7 @@ function BuscaDistrito() {
 	var res = false;
 	return new Promise(function (resolve, reject) {
 		try {
-			Comuna = Arr_Respuestas[1][1];
+			Comuna = Arr_Respuestas[1];
 			for (var cFil = 2; cFil < 347; cFil++) {
 				if (Comuna == Arr_Comunas[cFil][2]) {
 					Distr = Arr_Comunas[cFil][4]; //distrito del encuestado
@@ -132,10 +137,10 @@ function CalculaDistanciaCandidato(filp) {
 			//recorre todas las preguntas
 			if (Arr_Preguntas[cFilR][1].length != 0) {
 				//caso la pregunta es valida
-				if (Arr_Respuestas[cFilR + 1][1].length == 0) {
+				if (Arr_Respuestas[cFilR + 1].length == 0) {
 					NotaR = 99; //caso casilla vacía
 				} else {
-					NotaR = Arr_Respuestas[cFilR + 1][1]; //caso casilla buena
+					NotaR = Arr_Respuestas[cFilR + 1]; //caso casilla buena
 				}
 				Ponde = Arr_Preguntas[cFilR][6]; //lee ponderacion de  las pregunta
 				NotaC = Arr_Candidatos[filp][cFilR + FilToCol]; //lee nota individual del candidato
@@ -162,11 +167,11 @@ function CalculaDistanciaLista(filp) {
 			//recorre todas las preguntas
 			if (Arr_Preguntas[cFilR][1].length != 0) {
 				//caso la pregunta es valida
-				if (Arr_Respuestas[cFilR + 1][1].length == 0) {
+				if (Arr_Respuestas[cFilR + 1].length == 0) {
 					//Caso Respesta es valida
 					NotaR = 99; //caso casilla vacía
 				} else {
-					NotaR = Arr_Respuestas[cFilR + 1][1]; //caso casilla buena
+					NotaR = Arr_Respuestas[cFilR + 1]; //caso casilla buena
 				}
 				Ponde = Arr_Preguntas[cFilR][5]; //lee ponderacion de  las pregunta
 				NotaC = Arr_Candidatos[filp][cFilR + FilToCol]; //lee nota individual del candidato
@@ -199,19 +204,21 @@ function RecorreCandidatos() {
 			} // solo los candidatos del distrito
 			DisL = CalculaDistanciaLista(cFilC); //calcula distancia con lista
 			DisC = CalculaDistanciaCandidato(cFilC);
-			Arr_Resultado.push([
-				Arr_Candidatos[cFilC][4],
-				Arr_Candidatos[cFilC][5],
-				Arr_Candidatos[cFilC][6],
-				Arr_Candidatos[cFilC][7],
-				Arr_Candidatos[cFilC][8],
-				Arr_Candidatos[cFilC][9],
-				Arr_Candidatos[cFilC][14],
-				Arr_Candidatos[cFilC][0],
-				DisL,
-				DisC,
-			]);
-			cFilW++;
+			if (DisL < 100) {
+				Arr_Resultado.push([
+					Arr_Candidatos[cFilC][4],
+					Arr_Candidatos[cFilC][5],
+					Arr_Candidatos[cFilC][6],
+					Arr_Candidatos[cFilC][7],
+					Arr_Candidatos[cFilC][8],
+					Arr_Candidatos[cFilC][9],
+					Arr_Candidatos[cFilC][14],
+					Arr_Candidatos[cFilC][0],
+					DisL,
+					DisC,
+				]);
+				cFilW++;
+			}
 		} //proximo candidato
 		return cFilW;
 	} catch (e) {
@@ -226,6 +233,7 @@ function RecomiendaCandidato() {
 	var DCerca = new Array(5); //Distancia de los mas cercanos
 	var MaxFil; //fila maxima
 	var Arr_Salida = new Array();
+	var objeSalida = new Object();
 	var Arr_Res_Peor = new Array();
 	return new Promise(function (resolve, reject) {
 		MaxPreg = 45;
@@ -248,48 +256,120 @@ function RecomiendaCandidato() {
 						return a[9] - b[9];
 					});
 					//Ordena La escritura del archivo de Salida
+					// Agregar nombre de encuestado
+					objeSalida.name = name;
 					//Datos de comunas *****************************************************
 					Arr_Res_Peor.push(Arr_Resultado[Arr_Resultado.length - 1]); //Ultimo elemento guardado como el peor
 					Arr_Resultado.pop(); //Elimina ultimo elemento
-					Arr_Salida.push(['#', 'N Candidatos', 'N electros', 'Poblacion del distrito', 'Distrito']);
-					Arr_Salida.push([NCand, NCons, Pobl, Distr]);
-					//Encabezado resultados Mejor Candidato**********************************************************
-					Arr_Salida.push(['#', 'Mejores 3 Candidatos********************']);
-					Arr_Salida.push([
-						'#',
-						'Nombre1',
-						'Nombre2',
-						'Apellido1',
-						'Apellido2',
-						'Lista',
-						'Partido',
-						'Web',
-						'Codigo candidato',
-						'Dist L',
-						'Dist C',
-					]);
-					for (var i = 0; i < 3; i++) {
-						Arr_Salida.push(Arr_Resultado[i]);
+					objeSalida.informacion = { N_Candidatos: NCand, Cupos: NCons, Poblacion_del_distrito: Pobl, Distrito: Distr };
+					Arr_Salida.push({
+						informacion: { N_Candidatos: NCand, Cupos: NCons, Poblacion_del_distrito: Pobl, Distrito: Distr },
+					});
+					if (Arr_Resultado[0][8] + Arr_Resultado[0][9] > 18) {
+						Arr_Salida.push({ Coherencia: 'PIENSA TUS RESPUESTAS, PARECEN INCOHERENTES' });
+					} else {
+						Arr_Salida.push({ Coherencia: '' });
 					}
-					Arr_Salida.push(['# Peor Candidato********************']);
-					Arr_Salida.push([
-						'#',
-						'Nombre1',
-						'Nombre2',
-						'Apellido1',
-						'Apellido2',
-						'Lista',
-						'Partido',
-						'Web',
-						'Codigo candidato',
-						'Dist L',
-						'Dist C',
-					]);
-					Arr_Salida.push([Arr_Res_Peor[0]]);
+					//resultados Mejores Candidatos**********************************************************
+					objeSalida.mejoresCandidatos = [
+						{
+							Nombre1: Arr_Resultado[0][0],
+							Nombre2: Arr_Resultado[0][1],
+							Apellido1: Arr_Resultado[0][2],
+							Apellido2: Arr_Resultado[0][3],
+							Lista: Arr_Resultado[0][4],
+							Partido: Arr_Resultado[0][5],
+							Web: Arr_Resultado[0][6],
+							Codigo_candidato: Arr_Resultado[0][7],
+							Porcentaje_Cercania: PorcentajeCercania(Arr_Resultado[0][8], Arr_Resultado[0][9]),
+						},
+						{
+							Nombre1: Arr_Resultado[1][0],
+							Nombre2: Arr_Resultado[1][1],
+							Apellido1: Arr_Resultado[1][2],
+							Apellido2: Arr_Resultado[1][3],
+							Lista: Arr_Resultado[1][4],
+							Partido: Arr_Resultado[1][5],
+							Web: Arr_Resultado[1][6],
+							Codigo_candidato: Arr_Resultado[1][7],
+							Porcentaje_Cercania: PorcentajeCercania(Arr_Resultado[1][8], Arr_Resultado[1][9]),
+						},
+						{
+							Nombre1: Arr_Resultado[2][0],
+							Nombre2: Arr_Resultado[2][1],
+							Apellido1: Arr_Resultado[2][2],
+							Apellido2: Arr_Resultado[2][3],
+							Lista: Arr_Resultado[2][4],
+							Partido: Arr_Resultado[2][5],
+							Web: Arr_Resultado[2][6],
+							Codigo_candidato: Arr_Resultado[2][7],
+							Porcentaje_Cercania: PorcentajeCercania(Arr_Resultado[2][8], Arr_Resultado[2][9]),
+						},
+					];
+					Arr_Salida.push({
+						mejoresCandidatos: [
+							{
+								Nombre1: Arr_Resultado[0][0],
+								Nombre2: Arr_Resultado[0][1],
+								Apellido1: Arr_Resultado[0][2],
+								Apellido2: Arr_Resultado[0][3],
+								Lista: Arr_Resultado[0][4],
+								Partido: Arr_Resultado[0][5],
+								Web: Arr_Resultado[0][6],
+								Codigo_candidato: Arr_Resultado[0][7],
+								Porcentaje_Cercania: PorcentajeCercania(Arr_Resultado[0][8], Arr_Resultado[0][9]),
+							},
+							{
+								Nombre1: Arr_Resultado[1][0],
+								Nombre2: Arr_Resultado[1][1],
+								Apellido1: Arr_Resultado[1][2],
+								Apellido2: Arr_Resultado[1][3],
+								Lista: Arr_Resultado[1][4],
+								Partido: Arr_Resultado[1][5],
+								Web: Arr_Resultado[1][6],
+								Codigo_candidato: Arr_Resultado[1][7],
+								Porcentaje_Cercania: PorcentajeCercania(Arr_Resultado[1][8], Arr_Resultado[1][9]),
+							},
+							{
+								Nombre1: Arr_Resultado[2][0],
+								Nombre2: Arr_Resultado[2][1],
+								Apellido1: Arr_Resultado[2][2],
+								Apellido2: Arr_Resultado[2][3],
+								Lista: Arr_Resultado[2][4],
+								Partido: Arr_Resultado[2][5],
+								Web: Arr_Resultado[2][6],
+								Codigo_candidato: Arr_Resultado[2][7],
+								Porcentaje_Cercania: PorcentajeCercania(Arr_Resultado[2][8], Arr_Resultado[2][9]),
+							},
+						],
+					});
+					//Peor Candidato**********************************************************************************
+					objeSalida.peorCandidato = {
+						Nombre1: Arr_Res_Peor[0][0],
+						Nombre2: Arr_Res_Peor[0][1],
+						Apellido1: Arr_Res_Peor[0][2],
+						Apellido2: Arr_Res_Peor[0][3],
+						Lista: Arr_Res_Peor[0][4],
+						Partido: Arr_Res_Peor[0][5],
+						Web: Arr_Res_Peor[0][6],
+						Codigo_candidato: Arr_Res_Peor[0][7],
+						Porcentaje_Cercania: PorcentajeCercania(Arr_Res_Peor[0][8], Arr_Res_Peor[0][9]),
+					};
+					Arr_Salida.push({
+						peorCandidato: {
+							Nombre1: Arr_Res_Peor[0][0],
+							Nombre2: Arr_Res_Peor[0][1],
+							Apellido1: Arr_Res_Peor[0][2],
+							Apellido2: Arr_Res_Peor[0][3],
+							Lista: Arr_Res_Peor[0][4],
+							Partido: Arr_Res_Peor[0][5],
+							Web: Arr_Res_Peor[0][6],
+							Codigo_candidato: Arr_Res_Peor[0][7],
+							Porcentaje_Cercania: PorcentajeCercania(Arr_Res_Peor[0][8], Arr_Res_Peor[0][9]),
+						},
+					});
 					//guarda Peor****************************************************************************
-					//console.log(Arr_Salida)
-					EscribeArchivo('Resultado.csv', Arr_Salida);
-					resolve(Arr_Salida);
+					resolve(objeSalida);
 				}
 			} else {
 				console.log('No Se Encontro Distrito');
@@ -304,3 +384,7 @@ function GetResultado() {
 	return Arr_Resultado;
 }
 exports.GetResultado = GetResultado;
+function PorcentajeCercania(DistL, DistC) {
+	return (-2.9412 * (DistL + DistC) + 117).toFixed(1) + '%';
+}
+exports.PorcentajeCercania = PorcentajeCercania;
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,35 +3,18 @@ const path = require('path');
 const router = express.Router();
 
 const { getQuestions, getJSONFromString } = require('../helpers/handlebars');
-const functions = require('../helpers/funciones');
+const functions = require('../helpers/functions');
 
 let json = getQuestions();
 
-router.post('/', (req, res) => {
-	res.send('hola');
-	const obj = getJSONFromString(req.body);
-	console.log(Object.values(obj));
-});
-
-router.post('/process', (req, res) => {
+router.post('/process', async (req, res) => {
+	console.log(req.body);
 	const obj = getJSONFromString(req.body); //Parametro recibido en la solicitud
 	const arr = Object.values(obj);
-	console.log('Solicitud recibida');
-	console.log(Object.values(obj));
-	functions.ProcesaRespuestas(arr).then((resultado) => {
-		console.log(resultado);
-		res.json(resultado);
-	});
-	res.render('results');
-});
-
-router.post('/api/responses', (req, res) => {
-	res.send('hola');
-
-	let par_recibido = functions.leeArchivo(path.join(__dirname, './Respuestas.csv'));
-	functions.ProcesaRespuestas(par_recibido).then((resultado) => {
-		console.log(resultado);
-		res.json(resultado);
+	const resultado = await functions.ProcesaRespuestas(arr);
+	console.log(resultado);
+	res.render('results', {
+		resultado,
 	});
 });
 
