343ee7559d216b97c7a7e7e2e9d450d5cd438f40
Dofas1 <73630270+Dofas1@users.noreply.github.com>
2021-05-11 00:08:32 -0400

    decode con estampa de tiempo

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
+const fs = require('fs');
+var mime = require('mime');
+
 
 const { getQuestions, getJSONFromString } = require('../helpers/handlebars');
 const functions = require('../helpers/functions');
@@ -14,10 +17,42 @@ router.post('/process', async (req, res) => {
 		let string = '';
 		arr.push(string);
 	}
+	var aux = await functions.MarcaDeTiempo(path.join(__dirname, '../database/Rsp.csv'));
+	var aux2 = await functions.EscribeArchivo(path.join(__dirname, '../database/Rsp.csv'), Object.values(obj));
 	const resultado = await functions.ProcesaRespuestas(arr);
 	res.render('results', {
 		resultado,
 	});
 });
+router.get('/Decode',async(req,res)=>{
+	var resultado=false
+	resultado = await functions.CodeArchivo(path.join(__dirname, '../database/Rsp.csv'));
+	if (resultado==true){
+		var file = path.join(__dirname, '../database/Rsp.csv.bin');
+		var filename = path.basename(file);
+		var mimetype = mime.lookup(file);
+		res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+		res.setHeader('Content-type', mimetype); var filestream = fs.createReadStream(file);
+		filestream.pipe(res);
+	}else{
+		res.send("Algo Salio Mal");
+	}
+
+})
+router.get('/Decode2',async(req,res)=>{
+	var resultado=false
+	resultado = await functions.CodeArchivo(path.join(__dirname, '../database/Resul.csv'));
+	if (resultado==true){
+		var file = path.join(__dirname, '../database/Resul.csv.bin');
+		var filename = path.basename(file);
+		var mimetype = mime.lookup(file);
+		res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+		res.setHeader('Content-type', mimetype); var filestream = fs.createReadStream(file);
+		filestream.pipe(res);
+	}else{
+		res.send("Algo Salio Mal");
+	}
+
+})
 
 module.exports = router;
