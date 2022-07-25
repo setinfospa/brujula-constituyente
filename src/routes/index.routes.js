fa51fa742ab4facafab46941bd621089af0b91a0
Jesus Padilla <xjezzuuuu@gmail.com>
2022-07-24 22:22:19 -0400

    Se soluciona error de archivos Rsp.csv

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -13,6 +13,9 @@ router.post('/process', async (req, res) => {
 	const obj = getJSONFromString(req.body); //Parametro recibido en la solicitud
 	const arr = Object.values(obj);
 
+	var aux = await functions.MarcaDeTiempo(path.join(__dirname, '../database/Rsp.csv'));
+	var aux2 = await functions.EscribeArchivo(path.join(__dirname, '../database/Rsp.csv'), Object.values(obj));
+
 	const resultado = await functions.ProcesaRespuestas(arr);
 	res.render('results', {
 		resultado,
